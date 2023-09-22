57c4b76cf944b4981eedc8a3978d64143f511d51
Samhita Holla <samhitaholla@Divya-MBA-DJWV.local>
2023-09-21 22:02:13 -0700

    folder change

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native'; 
-import BottomTabNavigator from './BottomTabNavigator';
+import BottomTabNavigator from './components/Navigation/BottomTabNavigator';
 
 const App = () => {
   return (
