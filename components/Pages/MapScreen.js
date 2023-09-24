2533632d8ab404fe37cbed3fcf1e620f44e9d8e3
ljyt03 <litoby1203@gmail.com>
2023-09-24 00:30:19 -0700

    basic map

--- components/Pages/MapScreen.js
+++ components/Pages/MapScreen.js
@@ -1,13 +1,51 @@
 import React from 'react';
-import { View, Text, Button, Image, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 import { SafeAreaView } from 'react-navigation';
+
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 
 
 export const MapScreen = ({ navigation }) => {
-    return(
-        <SafeAreaView>
-            <Text>Map</Text>
+    return (
+        <SafeAreaView style={styles.container}>
+            <GooglePlacesAutocomplete
+                placeholder='Search'
+                onPress={(data, details = null) => {
+                    // 'details' is provided when fetchDetails = true
+                    console.log(data, details);
+                }}
+                query={{
+                    key: 'AIzaSyBpzps0uM3Rca6vZ7zICaPejuvZmyMtN78',
+                    language: 'en',
+                }}
+                styles={{
+                    container: {flex: 0, position: "absolute", width: "100%", zIndex: 1},
+                    listView: {backgroundColor: "white"}
+                }}
+            />
+            <MapView style={styles.map}
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+                provider= "google"
+            >
+            </MapView>
         </SafeAreaView>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: 50,
+    },
+    map: {
+      width: '100%',
+      height: '100%',
+    },
+  });
