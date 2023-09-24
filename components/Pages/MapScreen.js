62f19d203e9d9d102f9835ba20873f7dfc271777
ljyt03 <litoby1203@gmail.com>
2023-09-24 00:57:15 -0700

    fetch location and parks

--- components/Pages/MapScreen.js
+++ components/Pages/MapScreen.js
@@ -1,43 +1,75 @@
-import React from 'react';
+
 import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
-import { SafeAreaView } from 'react-navigation';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+
 
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
-
-
 export const MapScreen = ({ navigation }) => {
+    const [location, setLocation] = useState(null);
+    const [parks, setParks] = useState([]);
+
+    useEffect(() => {
+        const fetchLocationAndParks = async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                console.error('Permission to access location was denied');
+                return;
+            }
+
+            let currentLocation = await Location.getCurrentPositionAsync({});
+            setLocation(currentLocation.coords);
+
+            const nearbyParks = await fetchParks(currentLocation.coords);
+            setParks(nearbyParks);
+        };
+
+        fetchLocationAndParks();
+    }, []);
+
     return (
         <SafeAreaView style={styles.container}>
-            <GooglePlacesAutocomplete
-                placeholder='Search'
-                onPress={(data, details = null) => {
-                    // 'details' is provided when fetchDetails = true
-                    console.log(data, details);
-                }}
-                query={{
-                    key: '',
-                    language: 'en',
-                }}
-                styles={{
-                    container: {flex: 0, position: "absolute", width: "100%", zIndex: 1},
-                    listView: {backgroundColor: "white"}
-                }}
-            />
-            <MapView style={styles.map}
+            {/* <GooglePlacesAutocomplete
+                // ... your existing code
+            /> */}
+            <MapView 
+                style={styles.map}
                 initialRegion={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
+                    latitude: location?.latitude || 37.78825,
+                    longitude: location?.longitude || -122.4324,
                     latitudeDelta: 0.0922,
                     longitudeDelta: 0.0421,
                 }}
-                provider= "google"
+                provider="google"
             >
+                {parks.map((park, index) => (
+                    <Marker
+                        key={index}
+                        coordinate={{ latitude: park.lat, longitude: park.lng }}
+                        title={park.name}
+                    />
+                ))}
             </MapView>
         </SafeAreaView>
     );
-}
+};
+
+const fetchParks = async (coords) => {
+    const apiKey = ''; // Replace with your API key
+    const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coords.latitude},${coords.longitude}&radius=2000&type=park&key=${apiKey}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data.results.map(park => ({
+        lat: park.geometry.location.lat,
+        lng: park.geometry.location.lng,
+        name: park.name
+    }));
+};
 
 const styles = StyleSheet.create({
     container: {
