699edb1ba27dd41ee2041c9a170f829d363fd8fd
emilym <emily.markova@gmail.com>
2023-09-26 08:41:01 -0700

    randomize questions

--- components/Pages/QuestionScreen.js
+++ components/Pages/QuestionScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Dimensions } from "react-native";
 import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
 import QuestionHomeScreen from "./QuestionHomeScreen.js";
@@ -7,40 +7,28 @@ import BottomTabNavigator from "../Navigation/BottomTabNavigator.js";
 import './HomeScreen.js'
 
 export default function QuestionScreen({ navigation }) {
-  const [currentPage, setCurrentPage] = useState(1);
+  const [counter, setCurrentCounter] = useState(0);
 
-  async function questionAnswered(answerOfButtonPressed) {
-    if (screens[currentPage]["correctAnswer"] === answerOfButtonPressed) {
-      Alert.alert('Correct', 'That was correct!');
-      global.points += 1;
-    } else {
-      Alert.alert('Incorrect', 'That was incorrect!');
-    }
-    if (currentPage === 4) {
-      await new Promise(r=> setTimeout(r,1500));
-      Alert.alert('Finished', 'Congrats, you finished the quiz!');
-      global.completedQuiz = true;
-      navigation.navigate("Quiz");
-    } else {
-      setCurrentPage(currentPage + 1);
-    }
-  }
+  useEffect(() => {
+    setCurrentCounter(0); // This will run once when the component mounts
+  }, []);
+
   let screens = {
     1: {
-      question: "How much waste does the US produce annually?",
-      answer1: "268 million tons",
-      answer2: "137 million tons",
-      answer3: "323 million tons",
-      answer4: "1.2 trillion tons",
+      question: "What percentage of marine debris found from surface waters to deep-sea sediments does plastic make up?",
+      answer1: "80%",
+      answer2: "87%",
+      answer3: "75%",
+      answer4: "91%",
       correctAnswer: "answer1",
     },
     2: {
-      question: "How much plastic waste in the United States gets recycled?",
-      answer1: "8%",
-      answer2: "13%",
+      question: "What was the total bottle recycling rate for 2020?",
+      answer1: "27.2%%",
+      answer2: "13.3%",
       answer3: "5%",
-      answer4: "22%",
-      correctAnswer: "answer3",
+      answer4: "22.5%",
+      correctAnswer: "answer1",
     },
     3: {
       question: "How long does it take styrofoam to decompose?",
@@ -51,45 +39,103 @@ export default function QuestionScreen({ navigation }) {
       correctAnswer: "answer4",
     },
     4: {
-      question: "How many plastic bottles do Americans use every hour?",
-      answer1: "2.5 million",
-      answer2: "1.3 million",
-      answer3: "0.8 million",
-      answer4: "2.8 million",
+      question: "What percentage of textiles have the potential to be recycled or reused?",
+      answer1: "97%",
+      answer2: "91%",
+      answer3: "95%",
+      answer4: "81%",
+      correctAnswer: "answer3",
+    },
+    5: {
+      question: "Approximately how much of the food produced world wide is wasted and/or lost?",
+      answer1: "40%",
+      answer2: "30%",
+      answer3: "50%",
+      answer4: "45%",
+      correctAnswer: "answer2",
+    },
+    6: {
+      question: "Recycling cardboard only takes what percent of the energy required to make new cardboard?",
+      answer1: "75%",
+      answer2: "60%",
+      answer3: "84%",
+      answer4: "55%",
       correctAnswer: "answer1",
     },
+    7: {
+      question: "The total bottle recycling rate for 2019 was 28.7%. What was it in 2020?",
+      answer1: "25.6%",
+      answer2: "29.5%",
+      answer3: "28.4%",
+      answer4: "27.2%",
+      correctAnswer: "answer4",
+    },
+    8: {
+      question: "On average, how many pounds of waste per person goes into a landfill annually? ",
+      answer1: "500 pounds",
+      answer2: "1,500 pounds",
+      answer3: "1,000 pounds",
+      answer4: "2,500 pounds",
+      correctAnswer: "answer3",
+    },
   }
+
+  let array = [];
+  while (array.length < 5){
+    let newNumber = Math.floor(Math.random() * 8) + 1;
+    if (!array.includes(newNumber)) {
+      array.push(newNumber);
+    }
+  }
+
+  async function questionAnswered(answerOfButtonPressed) {
+    if (screens[array[counter]]["correctAnswer"] === answerOfButtonPressed) {
+      Alert.alert('Correct', 'That was correct!');
+      global.points += 1;
+    } else {
+      Alert.alert('Incorrect', 'That was incorrect!');
+    }
+    if (counter === 4) {
+      await new Promise(r=> setTimeout(r,1000));
+      Alert.alert('Finished', 'Congrats, you finished the quiz!');
+      global.completedQuiz = true;
+      navigation.navigate("Quiz");
+    } else {
+      setCurrentCounter(counter+1);
+    }
+  }
+
   return (
     <View style={styles.container}>
 			<Text style={styles.header}>Quiz</Text>
-			<Text>{screens[currentPage]["question"]}</Text>
+			<Text>{screens[array[counter]]["question"]}</Text>
 			
 			<TouchableHighlight style={styles.buttonStyling}>
         <Button
           color="green"
           onPress={()  => {questionAnswered("answer1")}}
-          title={screens[currentPage]["answer1"]}
+          title={screens[array[counter]]["answer1"]}
 				/>
 				</TouchableHighlight>
 				<TouchableHighlight style={styles.buttonStyling}>
 				<Button
           color="green"
           onPress={()  => {questionAnswered("answer2")}}
-          title={screens[currentPage]["answer2"]}
+          title={screens[array[counter]]["answer2"]}
 				/>
 				</TouchableHighlight>
 				<TouchableHighlight style={styles.buttonStyling}>
 				<Button
           color="green"
           onPress={()  => {questionAnswered("answer3")}}
-          title={screens[currentPage]["answer3"]}
+          title={screens[array[counter]]["answer3"]}
 				/>
 				</TouchableHighlight>
 				<TouchableHighlight style={styles.buttonStyling}>
 				<Button
           color="green"
           onPress={()  => {questionAnswered("answer4")}}
-          title={screens[currentPage]["answer4"]}
+          title={screens[array[counter]]["answer4"]}
         />
         </TouchableHighlight>
     </View>
